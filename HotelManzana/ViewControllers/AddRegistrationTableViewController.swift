e409365b6187040fcb6dafc33eaa41082d1322d5
Gordon Choi <thefirate@gmail.com>
2021-05-20 22:54:21 +0900

    Set up date picker to get checkin and checkout date

--- HotelManzana/ViewControllers/AddRegistrationTableViewController.swift
+++ HotelManzana/ViewControllers/AddRegistrationTableViewController.swift
@@ -12,13 +12,20 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
-
+    
+    @IBOutlet var checkInDateLabel: UILabel!
+    @IBOutlet var checkInDatePicker: UIDatePicker!
+    @IBOutlet var checkOutDateLabel: UILabel!
+    @IBOutlet var checkOutDatePicker: UIDatePicker!
+    
 }
 
 extension AddRegistrationTableViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
 
     }
 }
@@ -36,7 +43,17 @@ extension AddRegistrationTableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+}
 
+extension AddRegistrationTableViewController {
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+    }
 }
 
 extension AddRegistrationTableViewController {
@@ -44,10 +61,20 @@ extension AddRegistrationTableViewController {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
         
         print("DONE TAPPED")
         print("First name: \(firstName)")
         print("Last name: \(lastName)")
         print("email: \(email)")
+        print("Checkin: \(checkInDate)")
+        print("Checkout: \(checkOutDate)")
+    }
+}
+
+extension AddRegistrationTableViewController {
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateDateViews()
     }
 }
